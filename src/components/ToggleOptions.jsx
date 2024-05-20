4c0b7ab0d3ca5eedb0b24a6a76d2c006a78699f3
mithun20522 <mithun20522@iiitd.ac.in>
2024-05-20 19:29:43 +0530

    toggle option UI Done

---
+++ src/components/ToggleOptions.jsx
@@ -0,0 +1,9 @@
+const ToggleOptions = () => {
+  return (
+    <div className='p-6 mx-2 relative mt-2 rounded-lg sm:max-w-lg sm:mx-auto bg-gray-200'>
+        <div className="bg-slate-50 hover:bg-slate-100 cursor-pointer w-fit px-6 py-2 absolute top-1 rounded-xl left-20">Products</div>
+    </div>
+  )
+}
+
+export default ToggleOptions
--- src/pages/Vibe.jsx
+++ src/pages/Vibe.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header"
+import ToggleOptions from "../components/ToggleOptions"
 
 const Vibe = () => {
   return (
     <div>
         <Header/>
+        <ToggleOptions/>
     </div>
   )
 }
